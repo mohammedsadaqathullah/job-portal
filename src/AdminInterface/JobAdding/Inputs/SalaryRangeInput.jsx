d5c8859e19d6550094e1f84c950a6beb68f64481
Mohammed <mdsadaq.fed@gmail.com>
2025-01-17 16:56:41 +0530

    salry range debug

--- src/AdminInterface/JobAdding/Inputs/SalaryRangeInput.jsx
+++ src/AdminInterface/JobAdding/Inputs/SalaryRangeInput.jsx
@@ -7,7 +7,7 @@ const SalaryRangeInput = () => {
         <span className='salaryInputCont'>
             <p className='title'>Salary Range LPA</p>
             <span className="salaryInput">
-                <input type="number" min={2.5} max={35} placeholder="&darr;&uarr; &#8377;0"
+                <input type="number" min={2} max={35} placeholder="&darr;&uarr; &#8377;0"
                     value={salary}
                     onChange={(e) => setSalary(e.target.value)}
                     required />
